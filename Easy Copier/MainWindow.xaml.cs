3e9e1f5dcc2bf51ea0b4890273dbc29cc3af2099
asithniwantha <asithniwantha@gmail.com>
2026-08-06 23:49:40 +0530

    Sure! Please provide the list of code change descriptions so I can generate the commit message.

--- Easy Copier/MainWindow.xaml.cs
+++ Easy Copier/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using Easy_Copier.Views;
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
+using System;
+using System.IO;
 using WinRT.Interop;
 
 namespace Easy_Copier
@@ -17,6 +19,12 @@ namespace Easy_Copier
 
             if (appWindow != null)
             {
+                var iconPath = Path.Combine(AppContext.BaseDirectory, "Assets", "easy copier ico.ico");
+                if (File.Exists(iconPath))
+                {
+                    appWindow.SetIcon(iconPath);
+                }
+
                 appWindow.Resize(new Windows.Graphics.SizeInt32(1400, 900));
             }
 
