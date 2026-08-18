da7f320b8ebb678e567c82c4bd10356e364e0f5c
asithniwantha <asithniwantha@gmail.com>
2026-08-18 16:18:16 +0530

    import

--- Easy Copier/ViewModels/SettingsViewModel.cs
+++ Easy Copier/ViewModels/SettingsViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using Easy_Copier.Infrastructure;
 using Easy_Copier.Models;
 using Easy_Copier.Services;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
