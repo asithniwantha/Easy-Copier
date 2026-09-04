6e05f90043f8156412fc46c2336b1b460f32ec85
asithniwantha <asithniwantha@gmail.com>
2026-09-04 23:15:23 +0530

    Add System.Text namespace to using directives
    Added System.Text to the using directives in GameInfoDownloadService.Requirements.cs to enable relevant functionality.

--- Easy Copier/Services/GameInfoDownloadService.Requirements.cs
+++ Easy Copier/Services/GameInfoDownloadService.Requirements.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading;
